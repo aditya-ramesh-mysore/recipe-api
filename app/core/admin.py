40303eacdbc916b3fc11d05696bc95c95c1abbd9
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-06-22 20:27:46 -0400

    Added custom models and user admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_superuser',
+                    'is_staff'
+                )
+            }
+        ),
+        (
+            'Important dates',
+            {
+                'fields' : (
+                    'last_login',
+                )
+            }
+        )
+    )
+    readonly_fields = ['last_login',]
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'name',
+                    'password1',
+                    'password2',
+                    'is_active',
+                    'is_superuser',
+                    'is_staff',
+                )
+            }
+        ),
+    )
+
+admin.site.register(models.User, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, name, password=None, **extra_fields):
+        new_user = self.model(email=self.normalize_email(email), name=name, **extra_fields)
+        new_user.set_password(password)
+        new_user.is_active = True
+        new_user.save(using=self._db)
+
+        return new_user
+
+    def create_superuser(self, email, password):
+        new_user = self.model(email=self.normalize_email(email))
+        new_user.set_password(password)
+        new_user.is_active = True
+        new_user.is_superuser = True
+        new_user.is_staff = True
+        new_user.save(using=self._db)
+
+        return new_user
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    objects = UserManager()
+
