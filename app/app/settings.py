073486238ac5101a5173fd1179315b76e488e80e
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-06-30 22:40:15 -0400

    Create user with tests done

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "core",
     "rest_framework",
     "drf_spectacular",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- app/app/urls.py
+++ app/app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema-view'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema-view'), name='swagger_docs_view',),
+    path('api/user/', include('user.urls')),
 ]
