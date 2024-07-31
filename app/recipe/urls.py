e346824fb3b40b75989597c4d2d4b47ce236a867
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-07-30 23:07:48 -0400

    Image upload and retrieve done

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,4 +1,4 @@
-from recipe.views import RecipeViewSet, TagViewSet, IngredientListView, IngredientDetailView
+from recipe.views import RecipeViewSet, TagViewSet, IngredientListView, IngredientDetailView, RecipeImageView
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
@@ -8,8 +8,10 @@ router = DefaultRouter()
 router.register("recipes", RecipeViewSet, basename="recipe")
 router.register("tags", TagViewSet, basename="tag")
 
+recipe_image_url = [path('recipes/<int:id>/image/', RecipeImageView.as_view(), name="recipe-image")]
+
 ingredient_urls = [
     path('ingredients/', IngredientListView.as_view(), name="ingredient-list"),
     path('ingredients/<int:id>/', IngredientDetailView.as_view(), name="ingredient-detail"),
 ]
-urlpatterns = router.urls + ingredient_urls
+urlpatterns = router.urls + ingredient_urls + recipe_image_url
